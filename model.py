6582175ee6fbd66a139bc62e6d988e2740a0f58f
jawadbrahim <138779914+jawadbrahim@users.noreply.github.com>
2023-09-25 10:39:25 +0300

    Update model.py

--- model.py
+++ model.py
@@ -3,20 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Food(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)  
     title = db.Column(db.String(250)) 
-    description = db.Column(db.Text)  
-    وصف = db.Column(db.Text) 
+    description = db.Column(db.Text)   
     picture = db.Column(db.String(500))
     ingredients = db.Column(db.String(1000)) 
-    category = db.Column(db.String(250)) 
-    related = db.Column(db.String(250))  
 
-    def __init__(self, title, description, picture, ingredients, category, related, وصف):
+    def __init__(self, title, description, picture, ingredients,id):
         self.title = title
         self.description = description
         self.picture = picture
         self.ingredients = ingredients
-        self.category = category
-        self.related = related
-        self.وصف = وصف
+        self.id=id
